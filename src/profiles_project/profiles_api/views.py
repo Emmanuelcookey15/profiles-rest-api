f8242497285df67408ebe3518ffb822520c05143
Emmanuelcookey15 <Emmanuelcookey744@gmail.com>
2020-05-05 23:29:01 +0100

    Added search function to user profiles API

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -125,3 +125,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
