725e8379d2246833a482e116dd8b966842920fee
Emmanuelcookey15 <Emmanuelcookey744@gmail.com>
2020-05-05 14:46:18 +0100

    Added create, retrieve, update, partiel update and destroy methods to viewset created

--- src/profiles_project/profiles_api/urls.py
+++ src/profiles_project/profiles_api/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
+from django.conf.urls import include
 
 from . import views
 
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+
+
 urlpatterns = [
     url(r'^hello-view/', views.HelloApiView.as_view()),
+    url(r'', include(router.urls)),
 ]
